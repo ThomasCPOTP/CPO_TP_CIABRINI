901bb0adcf11eb70ad678352d623de5b348535d8
tciab <tciab@10.3.1.237>
2025-10-24 12:35:23 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+/**
+ * CE PROGRAMME AFFICHE UN MESSAGE
+ * 20/10/2025 premier TP - TP0
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+        System.out.println( " ciao le s  " ) ;
+                
+    }
+    
+}
