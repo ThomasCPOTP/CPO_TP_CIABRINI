e41b37735cae4832c05fc66375263fdaabd55add
tciab <tciab@10.3.1.237>
2025-11-02 17:13:04 +0100

    fin du premier exo sur le TP0 "Affichage d’un message"

--- TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -4,6 +4,8 @@
  */
 package tp0_.helloworld;
 
+import java.util.Scanner;
+
 /**
  * CE PROGRAMME AFFICHE UN MESSAGE
  * 20/10/2025 premier TP - TP0
@@ -16,7 +18,20 @@ public class TP0_HelloWorld {
     public static void main(String[] args) {
         System.out.println( " Bonjour " ) ;
         System.out.println( " ciao le s  " ) ;
-                
+        
+        String prenom ;
+        Scanner sc ;
+        sc = new Scanner (System.in) ;
+        System.out.println("Quel est votre prenom ?");
+        prenom = sc.nextLine(); 
+        
+        System.out.println("Bonjour " + prenom + " !");
+        /**
+         * j'ajoute la derniere ligne pour faire apparaitre la variable prenom 
+         * et qu'elle ne soit pas souligner en jaune avec ecrit que elle n'est jamais
+         * fin exercice1
+         */
+   
     }
     
 }
