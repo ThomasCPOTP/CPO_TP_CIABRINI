0ca9bb006c040b72311e7f1695c3842d9e553324
tciab <tciab@10.3.1.237>
2025-11-03 17:47:28 +0100

    TP2

---
+++ TP2_Bieres_CIABRINI/src/tp2_bieres_ciabrini/BouteilleBiere.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_ciabrini;
+
+/**
+ *
+ * @author tciab
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    // constructeur à 3 paramètres
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    // constructeur vide (utile si on veut pouvoir faire new BouteilleBiere();)
+    public BouteilleBiere() {
+        nom = "";
+        degreAlcool = 0.0;
+        brasserie = "";
+        ouverte = false;
+    }
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
+    }
+
+    public boolean decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.println("erreur : biere déjà ouverte");
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+        if (ouverte == true) chaine_a_retourner += "oui";
+        else chaine_a_retourner += "non";
+        return chaine_a_retourner;
+    }
+}
